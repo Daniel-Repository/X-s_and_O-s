3ca92e4221cb54bb85d9f04cc70c2303572ca865
Daniel Carroll <danielcarrollbusiness@gmail.com>
2022-03-02 20:12:27 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const squareTopLeft = document.getElementById('square-top-left');
+const squareTopMiddle = document.getElementById('square-top-middle');
+const squareTopRight = document.getElementById('square-top-right');
+const squareMiddleLeft = document.getElementById('square-middle-left');
+const squareMiddle = document.getElementById('square-middle-middle');
+const squareMiddleRight = document.getElementById('square-middle-right');
+const squareBottomLeft = document.getElementById('square-bottom-left');
+const squareBottomMiddle = document.getElementById('square-bottom-middle');
+const squareBottomRight = document.getElementById('square-bottom-right');
+
+const gameBoard = document.getElementById('game');
+const gameSquares = [];
+
+for (let i of gameBoard.children) {
+    gameSquares.push(i);
+}
+
+
+
+
